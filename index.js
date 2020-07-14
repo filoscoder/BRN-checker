2d743cefe1d1e559150d0508ea4b213f6046ffe5
filoscoder <sondaniel.88@gmail.com>
2020-07-14 16:18:46 +0900

    refactor: Changed file name and added htmlFTCParser

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const crnChecker = require("./CRNstatusChecker");
+const brnChecker = require("./BRNstatusChecker");
 
-// postCRN("3051577349").catch(err => console.log(err)).then(result => console.log(result))
-const CRNumber = process.argv[2]; // 매개변수 -> 사업자번호
-if (!CRNumber) {
+const BRN= process.argv[2]; // node index.js ${BRN}
+if (!BRN) {
   console.log("매개변수에 사업자등록번호를 입력하십시오");
 } else {
-crnChecker(CRNumber)
+brnChecker(BRN)
   .catch((err) => console.log(err))
   .then((result) => console.log(result));
 }
