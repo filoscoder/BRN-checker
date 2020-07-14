1ca6af2b0172c7ad5c44af3c24a07cc65705a9f4
filoscoder <sondaniel.88@gmail.com>
2020-07-14 16:18:58 +0900

    refactor: Moduled FTCkeys

---
+++ FTCkeys.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    "통신판매번호": "tcsNo",
+    "사업자등록번호": "brnNo",
+    "신고현황": "reportStatus",
+    "법인여부": "corporateType",
+    "상호": "companyName",
+    "대표자명": "representativeName",
+    "대표 전화번호": "tel",
+    "판매방식": "businessType",
+    "취급품목": "businessItem",
+    "전자우편(E-mail)": "email",
+    "신고일자": "reportDate",
+    "사업장소재지": "address1",
+    "사업장소재지(도로명)": "address2",
+    "인터넷도메인": "domain",
+    "호스트서버소재지": "serverAddress",
+    "휴페업상태": "businessStatus"
+}
